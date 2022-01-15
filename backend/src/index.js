e6bc88e30d66e15f1a0874f8f87506234df4870c
paudez14 <paudpez@hotmail.com>
2022-01-15 15:42:43 -0500

    Eliminacion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,41 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const flash = require('connect-flash');
-const PasanticApiPractices = require('./routes/practices');
-const PasanticApiAuth = require('./routes/aunthetication');
-const session = require('express-session');
-const MySqlSTORE= require('express-mysql-session');
-const passport = require('passport');
+const mysql = require('mysql');
+const PasanticApi = require('./routes/practices');
+//const session = require('express-session');
+//const MySqlSTORE= require('express-mysql-session');
+//const passport = require('passport');
 
-const {database}=require('./keys');
+//const {database}=require('./keys');
 
 //inicializaciones
 
 var app = express();
 require('./lib/passport');
+//configuraciones
+// app.set('port', process.env.PORT || 4000);
 //Funciones cada vez el cliente pide algo
-
+/*
 app.use(session({
-    secret: 'fool',
+    secret: 'fastmysqlnodessesion',
     resave: false,
     save: false,
     store: new MySqlSTORE(database)
 }));
-
+*/
 app.use(express.json());
 //para aunthetication
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 //Variables globales
 
+//Rutas 
+//app.use(require('./routes/index'));
+//app.use(require('./routes/aunthetication'));
 
-PasanticApiPractices(app);
-PasanticApiAuth(app);
+PasanticApi(app);
 //empezar el server 
 app.listen(3000, () => {
     console.log('Server on port3000');
--- backend/src/keys.js
+++ backend/src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'deathnote12P',
+        password: 'root',
         database: 'pasantic'
     }
 }
